ef089ff74f07fea4b1f61eb62377a9d7d51e2f93
MArjan123438 <marjanmaghsudi00@gmail.com>
2024-08-09 16:40:59 +0330

    update cw12

--- cw12.py
+++ cw12.py
@@ -1,32 +1,17 @@
-a=input().split(',')
-
-def fanck_1(x):
-    sum=0
-    for i in x:
-        sum+=int(i)
-    return sum/len(x)
-print(fanck_1(a))
-     
-#Function 3
+#Function 1,2,3
+numbers=input().split(',')
 
 def get_length_of_list(numbers):
     return len(numbers)
 
 def calculate_average(numbers):
-    if not numbers:  
-        return 0
-    total = sum(numbers)  
-    length = get_length_of_list(numbers)  
-    average = total / length  
-    return average
+    sum=0
+    for i in numbers:
+        sum+=int(i)
+    return sum/get_length_of_list(numbers)
 
-numbers = [1, 2, 3, 4, 5]
 average = calculate_average(numbers)
+length = get_length_of_list(numbers) 
+print("Length of the list:", length) 
 print("Average of the list:", average)
-# Function 2
-def get_length_of_list(numbers):
-    return len(numbers)
 
-numbers = [1, 2, 3, 4, 5]
-length = get_length_of_list(numbers)
-print("Length of the list:", length)
