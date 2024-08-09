284d3eee70985fa278db4ce078b0460993516991
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-08-09 05:25:43 -0700

    fanc1

--- cw12.py
+++ cw12.py
@@ -7,3 +7,10 @@ def fanck_1(x):
     return sum/len(x)
 print(fanck_1(a))
      
+# Function 2
+def get_length_of_list(numbers):
+    return len(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+length = get_length_of_list(numbers)
+print("Length of the list:", length)
